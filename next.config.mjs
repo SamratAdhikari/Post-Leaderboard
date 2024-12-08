09eea8ade0a6a532a6442e0fa4cbcfd3f1f083db
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-08 17:30:19 +0545

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: "export", // Enable static export
     reactStrictMode: true,
-    basePath: "/your-repo-name", // Replace 'your-repo-name' with your GitHub repository name
-    assetPrefix: "/your-repo-name/", // Ensure assets are correctly prefixed
+    basePath: "/Post-Leaderboard", // Replace 'your-repo-name' with your GitHub repository name
+    assetPrefix: "/Post-Leaderboard", // Ensure assets are correctly prefixed
 };
 
 export default nextConfig;
