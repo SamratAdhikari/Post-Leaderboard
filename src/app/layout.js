bc9da3391b5066ba5ead4d31ac63e7d0f11fc3a4
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-10 16:55:41 +0545

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,11 +29,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <Head>
-                <link
-                    rel="icon"
-                    href={path.resolve(process.cwd(), "public/favicon.ico")}
-                    type="image/x-icon"
-                />
+                <link rel="icon" href="./favicon.ico" type="image/x-icon" />
             </Head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} ${khand.variable} antialiased`}
